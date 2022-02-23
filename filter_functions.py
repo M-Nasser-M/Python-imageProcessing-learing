d58d52e8102a6507b5e792944bcad41ff75aa8c5
M_Nasser_M <momom_2011@hotmail.com>
2022-02-23 14:30:32 +0200

    add thumbnail

--- filter_functions.py
+++ filter_functions.py
@@ -1,4 +1,4 @@
-from PIL import Image,ImageFilter
+from PIL import Image, ImageFilter
 import re
 
 
@@ -9,3 +9,33 @@ def turn_to_grayscale(img_path: str, out_path):
     img = Image.open(img_path)
     img = img.convert('L')
     img.save(f'{out_path}/{new_name}')
+    return img
+
+
+def apply_blur_filter(img_path: str, out_path):
+    name = re.search("(?:.(?!\/))+$", img_path)
+    new_name = name.group().replace('/', 'blurred_').replace('jpg', 'png')
+
+    img = Image.open(img_path)
+    img = img.filter(ImageFilter.BLUR)
+    img.save(f'{out_path}/{new_name}')
+    return img
+
+
+def apply_crop(img_path, out_path, crop_size):
+    name = re.search("(?:.(?!\/))+$", img_path)
+    new_name = name.group().replace('/', 'cropped_').replace('jpg', 'png')
+
+    img = Image.open(img_path)
+    img = img.crop(crop_size)
+    img.save(f'{out_path}/{new_name}')
+    return img
+
+def apply_thumbnail(img_path , out_path , thumbnail_size):
+    name = re.search("(?:.(?!\/))+$", img_path)
+    new_name = name.group().replace('/', 'thumbnail_').replace('jpg', 'png')
+
+    img = Image.open(img_path)
+    img.thumbnail(thumbnail_size)
+    img.save(f'{out_path}/{new_name}')
+    return img
--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-from filter_to_grayscale import turn_to_grayscale
+from filter_functions import turn_to_grayscale, apply_blur_filter, apply_crop, apply_thumbnail
 import os
 directory = './assets'
 list = os.listdir(directory)
-print(list)
+
+# for item in list:
+#     turn_to_grayscale(f'{directory}/{item}', './out')
+
+# for item in list:
+#     apply_blur_filter(f'{directory}/{item}', './out')
+
+# for item in list:
+#     apply_crop(f'{directory}/{item}', './out', (100, 100, 400, 400))
+
 for item in list:
-    turn_to_grayscale(f'{directory}/{item}', './out')
+    apply_thumbnail(f'{directory}/{item}', './out', (400, 400))
